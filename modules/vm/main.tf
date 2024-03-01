aa489f06693dc9abc9c40f56427a906199cfd135
roshni patel <engr.roshnipatel@gmail.com>
2024-03-01 14:23:10 +0530

    added

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -12,6 +12,7 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
+
 # Define the public IP address
 resource "azurerm_public_ip" "public_ip" {
   name                = "${var.vm_name}-ip"
@@ -19,45 +20,48 @@ resource "azurerm_public_ip" "public_ip" {
   resource_group_name = var.resource_group_name
   allocation_method   = "Dynamic"
 }
-
-# Create a virtual machine
+resource "tls_private_key" "secureadmin_ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 resource "azurerm_linux_virtual_machine" "vm" {
   name                            = var.vm_name
   location                        = var.location_name
   resource_group_name             = var.resource_group_name
   size                            = var.vm_size
   admin_username                  = var.vm_username
-  admin_password                  = var.vm_password
-  disable_password_authentication = false
+  disable_password_authentication = true
   network_interface_ids           = [azurerm_network_interface.nic.id]
 
+   admin_ssh_key {
+    username   = "azureuser"
+    public_key = tls_private_key.secureadmin_ssh.public_key_openssh
+  }
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
 
   source_image_reference {
-  publisher = "Canonical"
-  offer     = "UbuntuServer"
-  sku       = "18.04-LTS"  
-  version   = "latest"
-}
-
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"  
+    version   = "latest"
+  }
 
   tags = {
     name  = "resource-owner"
     owner = "roshni-einfochips.com"
-  }  
-#custom_data = filebase64("${path.module}/azure_cli_login.sh")
+  }
 
- provisioner "local-exec" {
-  command = <<-EOT
-    az storage blob upload --account-name ${var.storage_account_name} \
-                            --container-name ${var.container_name}  \
-                            --name file1.txt \
-                            --type block \
-                            --content-type "text/plain" \
-                            --file ./file1.txt
-  EOT
-}
+  provisioner "local-exec" {
+    command = <<-EOT
+      az storage blob upload --account-name ${var.storage_account_name} \
+                              --container-name ${var.container_name}  \
+                              --name file1.txt \
+                              --type block \
+                              --content-type "text/plain" \
+                              --file ./file1.txt
+    EOT
+  }
 }
--- modules/vm/output.tf
+++ modules/vm/output.tf
@@ -5,3 +5,8 @@ output "vm_id" {
 output "storage_access_key" {
   value = azurerm_storage_account.storage_account.primary_access_key
 }*/
+
+output "tls_private_key" {
+  value     = tls_private_key.secureadmin_ssh.private_key_pem
+  sensitive = true
+}
